9ac6f729831660a2bbd7696cb2b5d5b360763cb1
LuvTian <565195693@qq.com>
2021-09-28 22:39:15 +0800

    first commit

---
+++ 02_src_ref属性/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h2 v-text="msg" ref="h2"></h2>
+        <button ref="btn" @click="show">点击</button>
+        <School ref="sch"></School>
+    </div>
+</template>
+
+<script>
+    import School from "./components/School"
+    export default {
+        name: "App",
+        components: {
+            School,
+        },
+        data() {
+            return {
+                msg: 'Hello, World'
+            }
+        },
+        methods: {
+            show(){
+                console.log(this.$refs)
+                console.log(this.$refs.h2) // h2真实DOM
+                console.log(this.$refs.sch) // 组件上的ref是指向的组件的实例对象(vc)
+                this.$refs.h2.style.color = 'red'
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Student.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p>{{name}}</p>
+        <p>{{address}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            name: '北京大学',
+            address: '北京市'
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
